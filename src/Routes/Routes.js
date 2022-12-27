9f99adc5e38fb77caa9ad47c16d8db58a0f7e3b4
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-12-27 15:36:45 +0600

    basic project setup and router setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import AddTask from "../Pages/AddTask/AddTask";
+import CompletedTask from "../Pages/CompletedTask/CompletedTask";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import MyTask from "../Pages/MyTask/MyTask";
+import SignUp from "../Pages/SignUp/SignUp";
+
+const router= createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/addTask',
+                element: <AddTask></AddTask>
+            },
+            {
+                path: '/myTask',
+                element: <MyTask></MyTask>
+            },
+            {
+                path: '/completedTask',
+                element: <CompletedTask></CompletedTask>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signUp',
+                element: <SignUp></SignUp>
+            }
+        ]
+    }
+]);
+
+export default router;
